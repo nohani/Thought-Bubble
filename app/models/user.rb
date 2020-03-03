e711f0a042bf40387ac1f7fd8d85c284626a6c3f
nohani <nicole.takuhi@gmail.com>
2020-03-02 19:26:05 -0500

    Troubleshooting

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   #FeGrip
   attr_reader :password
 
-  after_initialization :ensure_session_token
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password) 
      user = User.find_by(username: username)
