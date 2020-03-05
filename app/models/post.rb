164030c54fa1376e31406f72b019abde2d4a2e3f
nohani <nicole.takuhi@gmail.com>
2020-03-05 14:08:27 -0500

    AWS setup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,15 @@
 class Post < ApplicationRecord
   validates :author_id, presence: true
   validates :post_type, presence: true, inclusion: { in: %w(text photo quote link chat audio video) }
-  validates :content, presence: true, if: :has_this_type?
+  validates :content, presence: true, length: { minimum: 1}, if: :has_this_type?
 
   belongs_to :author, class_name: "User"
 
+  has_one_attached :photo
+
   def has_this_type?
     categories = %w(text quote chat)
-    categories.any? { |category| post_type == category }
+    categories.any? { |category| self.post_type == category }
   end
 
 end
--- app/views/api/posts/index.json.jbuilder
+++ app/views/api/posts/index.json.jbuilder
@@ -2,6 +2,7 @@
   json.posts do
     json.set! post.id do
       json.extract! post, :id, :post_type, :content
+      json.image_url <%= url_for(post.photo) =>
     end
   end
 end
